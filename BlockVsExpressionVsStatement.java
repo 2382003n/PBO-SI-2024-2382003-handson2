01c198d9633c0657bebccaadb4573f3b3c7b54ee
emman <2382003@unai.edu>
2024-09-10 10:13:09 +0700

    Handson2

---
+++ BlockVsExpressionVsStatement.java
@@ -0,0 +1,9 @@
+public class BlockVsExpressionVsStatement {
+    public static void main(String[] args) {
+        int a = 10 + 10;
+        System.out.println("Emmanuel Ruicosta Sitorus");
+        System.out.println("Emmanuel Ruicosta Sitorus");
+        System.out.println("Emmanuel Ruicosta Sitorus");
+        System.out.println("Emmanuel Ruicosta Sitorus");
+    }
+}
